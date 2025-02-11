64064d86f3aad51d54a4462eab16cfd0ded87493
Melanie Brown <mbrown9702@gmail.com>
2025-02-11 13:00:02 -0600

    work in progress

--- js/dropdown.js
+++ js/dropdown.js
@@ -15,3 +15,19 @@
       });
   });
   
+  document.addEventListener("DOMContentLoaded", function () {
+    const collapsibleButtons = document.querySelectorAll(".collapsible-btn");
+
+    collapsibleButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            let content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+            this.classList.toggle("active");
+        });
+    });
+});
+
